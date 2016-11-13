1524987dad60d5f33bfff6a089cdbd337f07e5d2
Ryan <Hall.Ryan.R@gmail.com>
2016-11-12 18:54:03 -0800

    update site

--- js/components/blurb.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({title, body, img}) => (
-  <div className="blurb">
-    <h4>{title}</h4>
-    <img src={"./public/img/" + img}/>
-    <p>{body}</p>
-  </div>
-);
--- js/components/portfolio.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div className="tab-container">Portfolio!!</div>
-);
